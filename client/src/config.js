90aa46c07b6608bf901245c4d4eb2d8f58c626d3
Piotr Słomczyński <wistako@gmail.com>
2024-05-15 16:44:59 +0200

    fix API_URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000/api';
+export const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000/api';
 export const IMGS_URL =
   process.env.NODE_ENV === 'production' ? '/api/images' : 'http://localhost:8000/api/images/';
 
